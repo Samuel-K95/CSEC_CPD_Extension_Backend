e54f59447cba35739fcb25cb02d067fc0e26e403
Samuel Keno <sgere195@gmail.com>
2025-09-20 00:17:02 +0300

    minor fix

--- app/main.py
+++ app/main.py
@@ -9,9 +9,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CSEC Contest Rating - Backend")
 
+origins = [
+    "*"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
